59ffabb13baf0c3b6cfd5f597a98ade712e9fe5c
Sergaav <sergaav@gmail.com>
2021-04-27 22:57:47 +0300

    Commit SAX

--- src/main/java/com/epam/rd/java/basic/practice7/parsers/STAXParser.java
+++ src/main/java/com/epam/rd/java/basic/practice7/parsers/STAXParser.java
@@ -16,26 +16,27 @@ public class STAXParser extends DefaultHandler {
 
     private final String xmlFileName;
 
-    private Shiporder shiporder;
+    private static Shiporder shiporder;
+    private static Order order = null;
+    private static Item item = null;
+    private static final List<Order> orders = new ArrayList<>();
+    private static final List<Item> items = new ArrayList<>();
 
     public Shiporder getShiporder() {
         return shiporder;
     }
+
 
     public STAXParser(String xmlFileName) {
         this.xmlFileName = xmlFileName;
     }
 
     public void parse() throws XMLStreamException {
-
-        Order order = null;
-        Item item = null;
-        List<Order> orders = new ArrayList<>();
-        List<Item> items = new ArrayList<>();
-
         String currentElement = null;
 
         XMLInputFactory factory = XMLInputFactory.newInstance();
+        factory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+        factory.setProperty("javax.xml.stream.isSupportingExternalEntities", false);
         factory.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
         factory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
 
@@ -54,69 +55,84 @@ public class STAXParser extends DefaultHandler {
             if (event.isStartElement()) {
                 StartElement startElement = event.asStartElement();
                 currentElement = startElement.getName().getLocalPart();
-
-                if (XMLTegs.SHIPORDER.equalsTo(currentElement)) {
-                    shiporder = new Shiporder();
-                }
-
-                if (XMLTegs.ORDER.equalsTo(currentElement)) {
-                    order = new Order();
-                }
-
-                if (XMLTegs.ITEM.equalsTo(currentElement)) {
-                    item = new Item();
-                }
+                startElementProcessing(currentElement);
             }
 
             if (event.isCharacters()) {
                 Characters characters = event.asCharacters();
-
-                if (XMLTegs.ORDERID.equalsTo(currentElement) && order != null) {
-                    order.setOrderid(characters.getData());
-                }
-                if (XMLTegs.NAME.equalsTo(currentElement) && order != null) {
-                    order.setName(characters.getData());
-                }
-                if (XMLTegs.ADDRESS.equalsTo(currentElement) && order != null) {
-                    order.setAddress(characters.getData());
-                }
-                if (XMLTegs.CITY.equalsTo(currentElement) && order != null) {
-                    order.setCity(characters.getData());
-                }
-                if (XMLTegs.COUNTRY.equalsTo(currentElement) && order != null) {
-                    order.setCountry(characters.getData());
-                }
-                if (XMLTegs.TITLE.equalsTo(currentElement) && item != null) {
-                    item.setTitle(characters.getData());
-                }
-                if (XMLTegs.QUANTITY.equalsTo(currentElement) && item != null) {
-                    item.setQuantity(Integer.parseInt(characters.getData()));
-                }
-                if (XMLTegs.PRICE.equalsTo(currentElement) && (item != null)) {
-                    item.setPrice(Double.parseDouble(characters.getData()));
-                }
-
+                charactersElementProcessing(currentElement, characters);
             }
 
             if (event.isEndElement()) {
                 EndElement endElement = event.asEndElement();
                 String localName = endElement.getName().getLocalPart();
+                endElementProcessing(localName);
 
-                if (XMLTegs.SHIPORDER.equalsTo(localName)) {
-                    shiporder.getOrders().addAll(orders);
-                }
-
-                if (XMLTegs.ORDER.equalsTo(localName) && order != null) {
-                    order.setItems(items);
-                    orders.add(order);
-                }
-                if (XMLTegs.ITEM.equalsTo(localName) && item != null) {
-                    items.add(item);
-                }
             }
         }
         reader.close();
     }
+
+    public static void startElementProcessing(String currentElement) {
+        if (XMLTegs.SHIPORDER.equalsTo(currentElement)) {
+            shiporder = new Shiporder();
+        }
+
+        if (XMLTegs.ORDER.equalsTo(currentElement)) {
+            order = new Order();
+        }
+
+        if (XMLTegs.ITEM.equalsTo(currentElement)) {
+            item = new Item();
+        }
+    }
+
+    public static void charactersElementProcessing(String currentElement, Characters characters) {
+        if (XMLTegs.ORDERID.equalsTo(currentElement) && order != null) {
+            order.setOrderid(characters.getData());
+        }
+        if (XMLTegs.NAME.equalsTo(currentElement) && order != null) {
+            order.setName(characters.getData());
+        }
+        if (XMLTegs.ADDRESS.equalsTo(currentElement) && order != null) {
+            order.setAddress(characters.getData());
+        }
+        if (XMLTegs.CITY.equalsTo(currentElement) && order != null) {
+            order.setCity(characters.getData());
+        }
+        if (XMLTegs.COUNTRY.equalsTo(currentElement) && order != null) {
+            order.setCountry(characters.getData());
+        }
+        itemsElementProcessing(currentElement, characters);
+    }
+
+    public static void itemsElementProcessing(String currentElement, Characters characters) {
+        if (XMLTegs.TITLE.equalsTo(currentElement) && item != null) {
+            item.setTitle(characters.getData());
+        }
+        if (XMLTegs.QUANTITY.equalsTo(currentElement) && item != null) {
+            item.setQuantity(Integer.parseInt(characters.getData()));
+        }
+        if (XMLTegs.PRICE.equalsTo(currentElement) && (item != null)) {
+            item.setPrice(Double.parseDouble(characters.getData()));
+        }
+    }
+
+
+    public static void endElementProcessing(String localName) {
+        if (XMLTegs.SHIPORDER.equalsTo(localName)) {
+            shiporder.getOrders().addAll(orders);
+        }
+
+        if (XMLTegs.ORDER.equalsTo(localName) && order != null) {
+            order.setItems(items);
+            orders.add(order);
+        }
+        if (XMLTegs.ITEM.equalsTo(localName) && item != null) {
+            items.add(item);
+        }
+    }
+
 
     public static void main(String[] args) throws Exception {
 
